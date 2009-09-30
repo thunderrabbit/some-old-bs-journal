f6192f90c961bf3908e44922cf5ef602b347c547
Rob Nugen <rob.git_MacBook@robnugen.com>
2009-09-30 10:17:21 +0900

    first bit of python code 	modified:   index.py

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
-<?php
-  /*  Planning to create a python version of my journal */
-?>
+#!/usr/bin/env python
+import cgi
+
+field = cgi.FieldStorage()
+print "Content-Type: text/plain\n\n"
+print 'Planning to create a python version of my journal, world!\n'
