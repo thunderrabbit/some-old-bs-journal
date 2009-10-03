aff954c3a5f3b6e55291c264b51ae41d4e528eee
Rob Nugen <rob.git_dreamhost@robnugen.com>
2009-10-03 08:03:57 -0700

    Not sure what changes are here 	modified:   index.py

--- index.py
+++ index.py
@@ -4,3 +4,9 @@ import cgi
 field = cgi.FieldStorage()
 print "Content-Type: text/plain\n\n"
 print 'Planning to create a python version of my journal, world!\n'
+
+with open('/home/thunderrabbit/robnugen.com/j/TODO', 'r') as todo_file:
+    read_data = todo_file.read()
+todo_file.closed
+    
+print read_data
